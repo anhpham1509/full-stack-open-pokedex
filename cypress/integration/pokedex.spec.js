6012c575a0b40307fc87350f9bcfa13c2c2e7d90
Anh Pham <duyanhpham1509@gmail.com>
2022-03-08 10:07:15 +0200

    fix port number in e2e test cases for exercise 11.9

--- cypress/integration/pokedex.spec.js
+++ cypress/integration/pokedex.spec.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8081')
+    cy.visit('http://localhost:500')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
@@ -8,7 +8,7 @@ describe('Pokedex', function () {
   })
 
   it('pokemon page can be navigated to', function () {
-    cy.visit('http://localhost:8081')
+    cy.visit('http://localhost:5000')
     cy.contains('charmander').click()
     cy.contains(
       'charmander'
